c26bb6b66dfe8ce6a71ac673df6b37396c711f36
Zhiyuan Zhang <zhiyuan.zhang2000@outlook.com>
2023-04-07 16:49:34 -0400

    add select course function, selected courses would be moved to the right bucket

--- js/api.js
+++ js/api.js
@@ -1,7 +1,13 @@
 // use an IIFE function to create a closure and encapsulate the implementation details of the API module
 // it creates a new scope for the code to run in
 const API = (() => {
-  const API_URL = "http://localhost:3000/courseList";
+  const API_URL = "http://localhost:3000/courses";
+
+  const getCourse = async (id) => {
+    // run `json-server db.json --id courseId` before start
+    const res = await fetch(`${API_URL}/${id}`);
+    return await res.json();
+  }
 
   const getCourseList = async () => {
     const res = await fetch(API_URL);
@@ -10,7 +16,8 @@ const API = (() => {
 
   const updateCourse = async (id, newCourse) => {
     // append id to url to specify which item to modify
-    const res = await fetch(`${API_URL}?courseId=${id}`, {
+    // run `json-server db.json --id courseId` before start
+    const res = await fetch(`${API_URL}/${id}`, {
       // replace the original course data
       method: "PUT",
       headers: {
@@ -24,21 +31,22 @@ const API = (() => {
   return {
     getCourseList,
     updateCourse,
+    getCourse
   }
 })();
 
-async function example() {
-  const newCourse = {
-    "courseId": 1,
-    "courseName": "Calculus",
-    "required": true,
-    "credit": 4
-  };
-  // API.updateCourse(1, newCourse).then(updatedCourse => {
-  //   console.log(updatedCourse);
-  // })
-  API.getCourseList().then(courses => {
-    console.log(courses);
-  })
-}
-example();
+// async function example() {
+//   const newCourse = {
+//     "courseId": 1,
+//     "courseName": "Calculus",
+//     "required": true,
+//     "credit": 3
+//   };
+//   API.updateCourse(1, newCourse).then(updatedCourse => {
+//     console.log("updated", updatedCourse);
+//   })
+//   API.getCourseList().then(courses => {
+//     console.log(courses);
+//   })
+// }
+// example();
--- js/script.js
+++ js/script.js
@@ -2,22 +2,53 @@ class CourseSelectionView {
   constructor() {
     this.selectionForm = document.querySelector(".course-selection__form");
     this.availableCourseList = document.querySelector(".course__list--available");
+    this.selectedCourseList = document.querySelector(".course__list--selected");
   }
 
   renderCourseLists(courseList) {
-    this.availableCourseList.innerText="";
+    this.availableCourseList.innerText = "";
     console.log("available course list", courseList);
     courseList.forEach((course) => {
       const courseItem = this.createCourseElement(course);
-      this.availableCourseList.appendChild(courseItem);
+      if (course.selected) {
+        this.selectedCourseList.appendChild(courseItem);
+      } else {
+        courseItem.addEventListener('click', () => {
+          courseItem.classList.toggle('course__item--selected');
+        })
+        this.availableCourseList.appendChild(courseItem);
+      }
     })
+  }
+
+  appendCourse(newCourse) {
+    const newItem = this.createCourseElement(newCourse);
+    this.selectedCourseList.appendChild(newItem);
+  }
+
+  removeCourse(id) {
+    console.log(this.availableCourseList);
+    const courses = this.availableCourseList.children;
+    console.log(courses);
+    for (let i = 0; i < courses.length; i++) {
+      const element = courses[i];
+      console.log('!!!',element);
+      const courseId = element.getAttribute('courseId');
+      if (courseId === id) {
+        element.remove();
+        return;
+      }
+    }
   }
 
   createCourseElement(newCourse) {
     const courseElem = document.createElement("div");
+    courseElem.classList.add("course__item");
+    courseElem.setAttribute('courseId', newCourse.courseId);
     const courseName = document.createElement("div");
     courseName.innerText = newCourse.courseName;
     const courseType = document.createElement("div");
+    courseElem.classList.add(`course__item--${newCourse.required ? "Compulsory" : "Elective"}`);
     courseType.innerText = `Course Type: ${newCourse.required ? "Compulsory" : "Elective"}`;
     const courseCredit = document.createElement("div");
     courseCredit.innerText = `Course Credit: ${newCourse.credit}`;
@@ -31,14 +62,23 @@ class CourseSelectionView {
 
 class CourseSelectionModel {
   #courseList;
+  #selectedCourseList;
   constructor() {
     this.#courseList = [];
+    this.#selectedCourseList = [];
   }
 
   async fetchCourseList() {
     const courseList = await API.getCourseList();
     this.courseList = courseList;
     return courseList;
+  }
+
+  async selectCourse(id) {
+    const originalCourse = await await API.getCourse(id);
+    const updatedCourse = await API.updateCourse(id, { ...originalCourse, selected: true });
+    this.#selectedCourseList.push(updatedCourse);
+    return updatedCourse;
   }
 }
 
@@ -54,6 +94,25 @@ class CourseSelectionController {
       const courseList = this.model.courseList;
       this.view.renderCourseLists(courseList);
     });
+
+    this.setUpSubmit();
+  }
+
+  setUpSubmit() {
+    console.log('!')
+    this.view.selectionForm.addEventListener('submit', (e) => {
+      console.log('!!')
+      e.preventDefault();
+      const selectedCourses = document.querySelectorAll(".course__item--selected");
+      console.log(selectedCourses);
+      selectedCourses.forEach(course => {
+        this.model.selectCourse(course.getAttribute('courseId')).then(newCourse => {
+          this.view.appendCourse(newCourse);
+          this.view.removeCourse(course.getAttribute('courseId'));
+          console.log(newCourse);
+        })
+      })
+    })
   }
 }
 
